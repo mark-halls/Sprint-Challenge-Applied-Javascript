147d92f2e78108b6ba81576da437370057891ea0
Mark Halls <mark.halls@gmail.com>
2019-10-04 09:37:16 -0700

    Add article cards component and add cards to page

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,42 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+const CardBuilder = article => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const headline = document.createElement("div");
+  headline.classList.add("headline");
+  headline.textContent = article.headline;
+  card.appendChild(headline);
+
+  const author = document.createElement("div");
+  const imgContainer = document.createElement("div");
+  const img = document.createElement("img");
+  const name = document.createElement("span");
+  author.classList.add("author");
+  imgContainer.classList.add("img-container");
+  img.src = article.authorPhoto;
+  name.textContent = `By ${article.authorName}`;
+  author.appendChild(imgContainer);
+  author.appendChild(name);
+  imgContainer.appendChild(img);
+  card.appendChild(author);
+
+  return card;
+};
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(res => {
+    const cardsContainer = document.querySelector(".cards-container");
+    const articles = res.data.articles;
+    const topics = Object.keys(articles);
+
+    topics.map(topic => {
+      articles[topic].map(article => {
+        cardsContainer.appendChild(CardBuilder(article));
+      });
+    });
+  })
+  .catch(err => console.error(err));
